a19fd66d714e29ccdac6411650476fc3e22d3b34
Jonathan Baker <jonathan@spatialnetworks.com>
2013-06-14 16:29:55 -0400

    Documentize!

--- MapBoxKit/MBKOfflineTileOverlay.h
+++ MapBoxKit/MBKOfflineTileOverlay.h
@@ -12,8 +12,18 @@
 
 @interface MBKOfflineTileOverlay : MKTileOverlay
 
-- (id)initWithTileProvider:(MBKOfflineTileProvider *)provider;
-
+/**
+ * The MBKOfflineTileProvider instance that will be providing the tiles.
+ */
 @property (nonatomic, strong) MBKOfflineTileProvider *provider;
+
+/**
+ * Initializes and returns a new tile overlay configured to use the given
+ * offline tile provider.
+ * \param provider An instance of an offline tile provider to serve the tiles.
+ * \returns Returns an initialized MBKOfflineTileOverlay object or nil if the
+ * object could not be successfully initialized.
+ */
+- (id)initWithTileProvider:(MBKOfflineTileProvider *)provider;
 
 @end
--- MapBoxKit/MBKOnlineTileOverlay.h
+++ MapBoxKit/MBKOnlineTileOverlay.h
@@ -12,10 +12,24 @@ extern NSString * const MBKDefaultMapBoxBaseURL;
 
 @interface MBKOnlineTileOverlay : MKTileOverlay
 
+/**
+ * The MapBox hosted map identifier.
+ */
 @property (nonatomic, strong) NSString *mapIdentifier;
 
+
+/**
+ * Whether or not the overlay will use HTTPS when requesting tiles.
+ */
 @property (nonatomic, assign) BOOL useSecureTransport;
 
+/**
+ * Initializes and returns a new tile overlay configured to use the given 
+ * MapBox map identifier.
+ * \param identifier An identifier for a MapBox hosted map.
+ * \returns Returns an initialized MBKOnlineTileOverlay object or nil if the 
+ * object could not be successfully initialized.
+ */
 - (id)initWithMapIdentifier:(NSString *)identifier;
 
 @end
